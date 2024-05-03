c4cbf9d4c3f40d10bf989a5bcbdfcf649448e9ae
Ahmed Elkhatib <131554633+el5hatib@users.noreply.github.com>
2024-05-03 09:51:32 +0300

    first problem
    we create method just SayHello

---
+++ WelcomeMethod.cs
@@ -0,0 +1,23 @@
+﻿using System; // System namespace       
+class Methods
+{
+    static void SayHello() // Method to print Hello World
+    {
+        Console.WriteLine("Hello World!");
+    }
+    static void Main() // Main method
+    {
+        SayHello(); // Call the SayHello method         
+    }
+}
+/*
+ *************** Method *************** 
+ *The method is a block of code that contains a series of statements.   
+ *We have three parts of a method:  
+ *(1) Method declaration: It defines the name, return type, and parameters of the method.   
+ *(2) Method body: It contains the code that defines the method.        
+ *(3) Method call: It is used to invoke the method.
+ *syntax:
+ *access-specifier return-type method-name(parameters)
+ *************** Eng / Ahmed Elkhatib ***************
+ */
