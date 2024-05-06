3749d1f38173ea7556ddf04596d15064b4596194
Ahmed Elkhatib <131554633+el5hatib@users.noreply.github.com>
2024-05-06 18:29:21 +0300

    convert from mile to kilometer

---
+++ MileToKilometer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Method15
+{
+    static double MileToKilometer(double mile) // Method to convert miles to kilometers
+    {
+        return mile * 1.6; // Return the value of miles multiplied by 1.60934
+    }
+    static void Main()
+    {
+        double mile;
+        Console.WriteLine("Enter a number of Mile");
+        mile = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("The number in kilometer is {0}",MileToKilometer(mile)); // Call the MileToKilometer method and print the result
+    
+    }
+}
