f8a22baf449b75e7baed71516789f478a1cf1d74
Ahmed Elkhatib <131554633+el5hatib@users.noreply.github.com>
2024-05-06 22:59:54 +0300

    Newton second equation of mottion

---
+++ Newton’sEquation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Methods19
+{
+    static void Main ()
+    {
+        double initialVelocity, acceleration, time, distance;
+        Console.WriteLine("Enter the initial velocity in m/s: ");
+        initialVelocity = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter the acceleration in m/s^2: ");
+        acceleration = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter the time in s: ");
+        time = Convert.ToDouble(Console.ReadLine());
+        distance = initialVelocity * time + 0.5 * acceleration * time * time;
+        Console.WriteLine("The distance is: " + distance);
+
+    }
+
+
+
+}
