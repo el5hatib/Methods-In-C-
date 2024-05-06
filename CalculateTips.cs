74237bac03eab5de601ff46e7b3a429496d4b292
Ahmed Elkhatib <131554633+el5hatib@users.noreply.github.com>
2024-05-06 22:12:41 +0300

    Calculate Tips

---
+++ CalculateTips.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Method16
+{
+
+  
+    static void Main()
+    {
+        double subtotal, gratuityRate, gratuity, total; 
+        Console.WriteLine("Enter the subtotal and a gratuity rate: "); 
+        subtotal = Convert.ToDouble(Console.ReadLine());    
+        gratuityRate = Convert.ToDouble(Console.ReadLine());
+        gratuity = subtotal * gratuityRate / 100;   
+        total = subtotal + gratuity;
+        Console.WriteLine("The gratuity is $" + gratuity + " and total is $" + total);  
+
+    }
+}*/
